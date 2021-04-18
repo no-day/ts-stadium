4cb890a312daff190f2d4dc89ef26250f737124b
Michael Bock <me@thought2.de>
2021-04-18 15:22:17 +0200

    Add example

--- README.md
+++ README.md
@@ -4,11 +4,16 @@ Write typesafe statemachines and get them visualized.
 
 <!-- AUTO-GENERATED-CONTENT:START (TOC) -->
 - [Docs](#docs)
+- [Demo](#demo)
 - [Features](#features)
 <!-- AUTO-GENERATED-CONTENT:END -->
 
 ## Docs
 
 [API Docs](https://no-day.github.io/ts-stadium)
+
+## Demo
+
+[API Docs](https://no-day.github.io/ts-stadium/demo)
 
 ## Features
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,49 @@
 /** @since 1.0.0 */
 
 // --------------------------------------------------------------------------------------------------------------------
-// Greetings
+// Util
 // --------------------------------------------------------------------------------------------------------------------
 
 /**
  * It's a greeting
  *
  * @since 1.0.0
- * @category Greetings
+ * @category Util
  * @example
- *   import { greet } from 'fp-ts-library-template';
- *   assert.deepStrictEqual(greet('World'), 'Hello, World!');
+ *   import { createStateMachine } from 'fp-ts-library-template';
+ *
+ *   createStateMachine()({
+ *     states: { On: { events: ['Toggle'] }, Off: { events: ['Toggle'] } },
+ *     events: { Toggle: { toStates: ['On', 'Off'] } },
+ *   });
  */
-export const greet = (name: string) => `Hello, ${name}!`;
+
+export const createStateMachine = <T extends TSpec>() => <spec extends Spec<T>>(spec: spec): StateMachine<T> =>
+  (spec as unknown) as StateMachine<T>;
+
+// --------------------------------------------------------------------------------------------------------------------
+// Internal
+// --------------------------------------------------------------------------------------------------------------------
+
+type TSpec = { states: Record<any, any>; events: Record<any, any> };
+
+type Spec<T extends TSpec> = {
+  states: Record<keyof T['states'], { events: (keyof T['events'])[] }>;
+  events: Record<keyof T['events'], { toStates?: (keyof T['states'])[]; toEvents?: (keyof T['events'])[] }>;
+};
+
+type StateMachine<T extends TSpec> = {
+  states: {
+    [key in keyof T['states']]: {
+      data: T['states'][key];
+      events: (keyof T['events'])[];
+    };
+  };
+  events: {
+    [key in keyof T['events']]: {
+      data: T['events'][key];
+      toStates?: (keyof T['states'])[];
+      toEvents?: (keyof T['events'])[];
+    };
+  };
+};
