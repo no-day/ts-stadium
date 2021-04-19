3f49999784af0ebbd7c4af6253cd2003263b9fd4
Michael Bock <me@thought2.de>
2021-04-19 06:44:23 +0200

    Fix demo

--- demo/src/pages/switch.tsx
+++ demo/src/pages/switch.tsx
@@ -1,13 +1,22 @@
 import * as React from "react"
 import * as Switch from "../../../examples/switch/src"
-import * as SM from "../../../src"
+import { createStateMachine } from "../../../src"
+import { createGraph } from "../../../src/graph"
+import { renderDot } from "../../../src/dot"
 import { Graphviz } from "graphviz-react"
 import { pipe } from "fp-ts/function"
 
 const Page = () => (
   <div>
     Page switch
-    <Graphviz dot={pipe(Switch.stateMachine, SM.toGraph, SM.graphToDot)} />
+    <Graphviz
+      dot={pipe(
+        Switch.stateMachine,
+        createStateMachine(),
+        createGraph,
+        renderDot
+      )}
+    />
   </div>
 )
 
