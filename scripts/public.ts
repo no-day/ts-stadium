e06f939373dd3a99c9b4ab19e82ec27f90746a6a
Michael Bock <me@thought2.de>
2021-04-29 06:04:07 +0200

    change public script

--- scripts/public.ts
+++ scripts/public.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as cp from 'child_process';
+import { pipe } from 'fp-ts/function';
 
 if (cp.execSync('git status --porcelain').toString() !== '') {
   console.error('Git working directory not clean');
@@ -8,10 +9,22 @@ if (cp.execSync('git status --porcelain').toString() !== '') {
 
 fs.rmdirSync('public', { recursive: true });
 
-const tag = cp.execSync('git describe --match "@ts-stadium/core@*" HEAD');
+const genDocs = (workspace: string) => {
+  const tag = cp.execSync(`git describe --match "${workspace}@*" HEAD`);
+  cp.execSync(`git checkout ${tag}`);
+  cp.execSync('yarn install');
+  cp.execSync(`yarn workspace ${workspace} docs-ts`);
+  const version = pipe(
+    fs.readFileSync('package.json'),
+    (_) => _.toString(),
+    JSON.parse,
+    (_) => _.version
+  );
+  const workspaces = () =>
+    pipe(cp.execSync('yarn workspaces info'), (_) => _.toString(), JSON.parse);
+  cp.execSync(
+    `cp -r ${workspaces[workspace].location}/docs public/docs/${workspace}/${version}`
+  );
+};
 
-//cp.execSync(`git checkou ${tag}`);
-
-//const tag = cp.spawn('git describe --match "@ts-stadium/core@*" HEAD');
-
-console.log(tag.toString());
+genDocs('@ts-stadium/core');
