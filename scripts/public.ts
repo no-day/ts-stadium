677b6f5c5e550d56579a31f7e251f8efb34545c7
Michael Bock <me@thought2.de>
2021-04-29 06:04:07 +0200

    change public script

--- scripts/public.ts
+++ scripts/public.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as cp from 'child_process';
+import * as path from 'path';
 import { pipe } from 'fp-ts/function';
 
 if (cp.execSync('git status --porcelain').toString() !== '') {
@@ -9,22 +10,30 @@ if (cp.execSync('git status --porcelain').toString() !== '') {
 
 fs.rmdirSync('public', { recursive: true });
 
+cp.execSync('mkdir -p public/docs');
+
 const genDocs = (workspace: string) => {
   const tag = cp.execSync(`git describe --match "${workspace}@*" HEAD`);
   cp.execSync(`git checkout ${tag}`);
   cp.execSync('yarn install');
   cp.execSync(`yarn workspace ${workspace} docs-ts`);
+  const workspaces = pipe(
+    cp.execSync('yarn workspaces info'),
+    (_) => _.toString(),
+    JSON.parse
+  );
+  const workspaceLocation = workspaces[workspace].location;
   const version = pipe(
-    fs.readFileSync('package.json'),
+    fs.readFileSync(path.join(workspaceLocation, `package.json`)),
     (_) => _.toString(),
     JSON.parse,
     (_) => _.version
   );
-  const workspaces = () =>
-    pipe(cp.execSync('yarn workspaces info'), (_) => _.toString(), JSON.parse);
-  cp.execSync(
-    `cp -r ${workspaces[workspace].location}/docs public/docs/${workspace}/${version}`
-  );
+  const targetDir = `public/docs/${workspace}/${version}`;
+  cp.execSync(`mkdir -p ${targetDir}`);
+  cp.execSync(`cp -r ${workspaceLocation}/docs/* ${targetDir}`);
 };
 
 genDocs('@ts-stadium/core');
+
+cp.execSync(`git checkout HEAD`);
