150993c31b501f7c677972fe06383d18151b8a5e
Michael Bock <me@thought2.de>
2021-05-04 09:38:17 +0200

    Add logging to public script

--- scripts/public.ts
+++ scripts/public.ts
@@ -1,10 +1,10 @@
-import * as fs from 'fs';
-import frontMatter_ = require('front-matter');
-import * as front_matter from 'front-matter';
-import * as cp from 'child_process';
-import * as path from 'path';
-import { pipe } from 'fp-ts/function';
-import { glob } from 'glob';
+import * as fs from 'fs'
+import frontMatter_ = require('front-matter')
+import * as front_matter from 'front-matter'
+import * as cp from 'child_process'
+import * as path from 'path'
+import { pipe } from 'fp-ts/function'
+import { glob } from 'glob'
 
 const getWorkspaces = (): Record<string, { location: string }> =>
   pipe(
@@ -14,49 +14,51 @@ const getWorkspaces = (): Record<string, { location: string }> =>
     (_) => _.split('\n'),
     (_) => _.map((s) => JSON.parse(s)),
     (_) => _.reduce((acc, x) => ({ ...acc, [x.name]: x }), {})
-  );
+  )
 
 const frontMatter = (frontMatter_ as unknown) as (
   str: string
-) => front_matter.FrontMatterResult<any>;
+) => front_matter.FrontMatterResult<any>
 
-const CHECKOUT_LATEST = false;
-const SKIP_DEMO = true;
+const CHECKOUT_LATEST = false
+const SKIP_DEMO = true
 
 const genDocs = (workspace: string) => {
+  console.log(`Generate docs for ${workspace} ...`)
   if (CHECKOUT_LATEST) {
-    const tag = cp.execSync(`git describe --match "${workspace}@*" HEAD`);
-    cp.execSync(`git checkout ${tag}`);
-    cp.execSync('yarn install');
+    const tag = cp.execSync(`git describe --match "${workspace}@*" HEAD`)
+    cp.execSync(`git checkout ${tag}`)
+    cp.execSync('yarn install')
   }
-  cp.execSync(`yarn workspace ${workspace} docs`);
+  const output = cp.execSync(`yarn workspace ${workspace} docs`)
+  console.log(output.toString())
 
-  const workspaceLocation = getWorkspaces()[workspace].location;
+  const workspaceLocation = getWorkspaces()[workspace].location
 
   const version = pipe(
     fs.readFileSync(path.join(workspaceLocation, `package.json`)),
     (_) => _.toString(),
     JSON.parse,
     (_) => _.version
-  );
-  const unscopedName = workspace.split('/')[1];
-  const targetDir = `public/packages/${unscopedName}`;
-  cp.execSync(`mkdir -p ${targetDir}`);
+  )
+  const unscopedName = workspace.split('/')[1]
+  const targetDir = `public/packages/${unscopedName}`
+  cp.execSync(`mkdir -p ${targetDir}`)
   pipe(
     { version },
     (_) => JSON.stringify(_, null, 2),
     (_) => fs.writeFileSync(path.join(targetDir, 'version.json'), _)
-  );
+  )
   cp.execSync(
     `cp -r ${path.join(workspaceLocation, 'docs/modules/*')} ${targetDir}`
-  );
+  )
   cp.execSync(
     `mv ${path.join(targetDir, 'index.ts.md')} ${path.join(
       targetDir,
       'index.md'
     )}`
-  );
-  const files = glob.sync(path.join(targetDir, '*.md'));
+  )
+  const files = glob.sync(path.join(targetDir, '*.md'))
   files.forEach((file) => {
     pipe(
       fs.readFileSync(file),
@@ -88,58 +90,59 @@ const genDocs = (workspace: string) => {
             result.body
           }`
         )
-    );
-  });
-  cp.execSync(`git checkout main`);
-};
+    )
+  })
+  cp.execSync(`git checkout main`)
+  console.log('done')
+}
 
 const genDemo = (workspace: string) => {
   if (CHECKOUT_LATEST) {
-    const tag = cp.execSync(`git describe --match "${workspace}@*" HEAD`);
-    cp.execSync(`git checkout ${tag}`);
-    cp.execSync('yarn install');
+    const tag = cp.execSync(`git describe --match "${workspace}@*" HEAD`)
+    cp.execSync(`git checkout ${tag}`)
+    cp.execSync('yarn install')
   }
-  cp.execSync(`yarn workspace ${workspace} build --prefix-paths`);
-  const workspaceLocation = getWorkspaces()[workspace].location;
+  cp.execSync(`yarn workspace ${workspace} build --prefix-paths`)
+  const workspaceLocation = getWorkspaces()[workspace].location
   const version = pipe(
     fs.readFileSync(path.join(workspaceLocation, `package.json`)),
     (_) => _.toString(),
     JSON.parse,
     (_) => _.version
-  );
-  const targetDir = `public/demo`;
-  cp.execSync(`mkdir -p ${targetDir}`);
+  )
+  const targetDir = `public/demo`
+  cp.execSync(`mkdir -p ${targetDir}`)
   pipe(
     { version },
     (_) => JSON.stringify(_, null, 2),
     (_) => fs.writeFileSync(path.join(targetDir, 'version.json'), _)
-  );
-  cp.execSync(`cp -r ${path.join(workspaceLocation, 'public/*')} ${targetDir}`);
-  cp.execSync(`git checkout main`);
-};
+  )
+  cp.execSync(`cp -r ${path.join(workspaceLocation, 'public/*')} ${targetDir}`)
+  cp.execSync(`git checkout main`)
+}
 
 const main = () => {
   if (
     CHECKOUT_LATEST &&
     cp.execSync('git status --porcelain').toString() !== ''
   ) {
-    console.error('Git working directory not clean');
-    process.exit(1);
+    console.error('Git working directory not clean')
+    process.exit(1)
   }
 
-  cp.execSync('mkdir -p public');
-  cp.execSync('rm -rf public/*');
-  cp.execSync('mkdir -p public/packages');
+  cp.execSync('mkdir -p public')
+  cp.execSync('rm -rf public/*')
+  cp.execSync('mkdir -p public/packages')
 
-  genDocs('@ts-stadium/graph');
-  genDocs('@ts-stadium/type-utils');
-  genDocs('@ts-stadium/state-machine');
+  genDocs('@ts-stadium/graph')
+  genDocs('@ts-stadium/type-utils')
+  genDocs('@ts-stadium/state-machine')
 
   if (!SKIP_DEMO) {
-    genDemo('@ts-stadium/demo');
+    genDemo('@ts-stadium/demo')
   }
 
-  cp.execSync('cp -r docs/* -t public');
-};
+  cp.execSync('cp -r docs/* -t public')
+}
 
-main();
+main()
